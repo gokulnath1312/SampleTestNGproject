959efbbe91b7e1857f7c5fc3d21e1e9b0e700074
GokulNath <GokulNath@Gokulnath>
2023-07-17 23:24:06 -0500

    TestNG initial commit

---
+++ SampleTestNGproject/src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,20 @@
+package testScripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GooglePageTest {
+  @Test
+  public void javaSearchTest() {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("github");
+		
+  }
+}
